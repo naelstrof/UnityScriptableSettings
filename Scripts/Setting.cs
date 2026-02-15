84f20148e3cc7ee819351210c34613473a72f44e
naelstrof <naelstrof@gmail.com>
2026-02-15 14:57:53 -0700

    Localization more optional now.

--- Scripts/Setting.cs
+++ Scripts/Setting.cs
@@ -5,23 +5,9 @@ using UnityEngine.Serialization;
 
 namespace UnityScriptableSettings {
     public abstract class Setting : ScriptableObject {
-        [Tooltip("Label of the option. Can be null")]
-        [SerializeField,FormerlySerializedAs("localizedName")] private LocalizedString localizedLabel;
-        [Tooltip("Backup label of the option. Only necessary if the localized label is null.")]
-        [SerializeField] private string backupLabel;
-        public string GetLabel() {
-            if (localizedLabel == null || localizedLabel.IsEmpty) {
-                if (string.IsNullOrEmpty(backupLabel)) {
-                    return name;
-                }
-                return backupLabel;
-            }
-            return localizedLabel.GetLocalizedString();
-        }
-        
-        public bool TryGetLocalizedLabel(out LocalizedString localizedLabel) {
-            localizedLabel = this.localizedLabel;
-            return this.localizedLabel != null;
+        [SerializeField] private ScriptableSettingString label;
+        public ScriptableSettingString GetLabel() {
+            return label;
         }
         
         [Tooltip("Name of the group that the setting belongs to (audio, graphics, gameplay...")]
